08e6ebbdb5451042d5bd946350c6c2a0e7876b9d
Muhammad Irfan <contact@irfanm.com>
2019-03-20 11:52:49 +1100

    update

--- Supervised Learning/Linear Regression/Linear Regression with One Variable/Octave/gradientDescent.m
+++ Supervised Learning/Linear Regression/Linear Regression with One Variable/Octave/gradientDescent.m
@@ -1,32 +1,26 @@
-% Gradient descent is used to minimize cost function J
-% Implementation for single variable/feature 
-
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+% Gradient descent is used to minimize cost function J
+% Vectorisation implementation. Applicable for any no for features. 
+% Theta should be a nx1 vector where n = No of feature + 1  
+
 %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
 % Algorithm
-% theta_0= theta_0 - alpha * (1/m) Sum [i=1 to m] (h(x)-y)*x_0 
-% theta_1= theta_1 - alpha * (1/m) Sum [i=1 to m] (h(x)-y)*x_1
-%    where x_0=1 
+% theta_i= theta_i - alpha * (1/m) Sum [i=1 to m] (h(x)-y)*x_i 
+%    where i= training data 
 
-% Vectorization implementation which is more efficient 
 
-m = size(X,1); % number of training examples
+m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    h_x = X * theta; % computing h(x) 
-    sqrErrors = h_x-y;
-    theta_0 = theta(1);
-    theta_1 = theta(2);
-    
-    temp_0 = theta_0 - alpha * (1/m) * sum(sqrErrors .* X(:,1));
-    temp_1 = theta_1 - alpha * (1/m) * sum(sqrErrors .* X(:,2));
- 
-    theta = [temp_0;temp_1];
+    hx = X * theta;
+    sqrErrors = hx - y;
+    newX = sqrErrors' * X;
+    theta = theta - ((alpha/m) * newX');
       
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
