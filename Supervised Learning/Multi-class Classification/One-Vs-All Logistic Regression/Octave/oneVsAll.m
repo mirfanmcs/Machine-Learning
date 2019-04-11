e2af34302276c96defe877677ed37054c85b8059
Muhammad Irfan <contact@irfanm.com>
2019-04-12 08:19:54 +1000

    update

--- Supervised Learning/Multi-class Classification/One-Vs-All Logistic Regression/Octave/oneVsAll.m
+++ Supervised Learning/Multi-class Classification/One-Vs-All Logistic Regression/Octave/oneVsAll.m
@@ -38,7 +38,6 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 for c = 1:num_labels
 	[theta] = fmincg (@(t)(costFunctionReg(t, X, (y == c), lambda)), initial_theta, options);
-	%[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, (y == c), lambda)), initial_theta, options);
     all_theta(c,:) = theta';
 endfor
 
