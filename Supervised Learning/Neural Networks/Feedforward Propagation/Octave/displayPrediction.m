94df5832d7dd77343194639220c2c294c98daf60
Muhammad Irfan <contact@irfanm.com>
2019-04-17 13:19:57 +1000

    update

--- Supervised Learning/Neural Networks/Feedforward Propagation/Octave/displayPrediction.m
+++ Supervised Learning/Neural Networks/Feedforward Propagation/Octave/displayPrediction.m
@@ -6,15 +6,14 @@ m = size(X, 1);
 rp = randperm(m);
 
 
-
 for i = 1:m
       % Display 
       fprintf('\nDisplaying Example Image\n');
       
-	  displayData(X(rp(i), :));
 
       pred = predict(Theta1, Theta2, X(rp(i),:));
-      fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+	    displayData(X(rp(i), :),true,strcat('./predict-images/', num2str(pred),'.png'));
+      fprintf('\nPrediction: %d (digit %d)\n', pred, mod(pred, 10));
       
       % Pause with quit option
       s = input('Paused - press enter to continue, q to exit:','s');
