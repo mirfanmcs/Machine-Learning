259902951c1069ae2caea68128da13e06d1d4e68
Muhammad Irfan <contact@irfanm.com>
2019-04-19 12:45:30 +1000

    update

--- Supervised Learning/Multi-class Classification/One-Vs-All Logistic Regression/Python/predictImg.py
+++ Supervised Learning/Multi-class Classification/One-Vs-All Logistic Regression/Python/predictImg.py
@@ -1,14 +1,15 @@
-import loadData as data
 import numpy as np
 import predictOneVsAll as pred
-import scipy.misc
+# import scipy.misc
+import imageio
 
 import oneVsAll as onevAll
 
 
 def predictImg(Theta, Img):
 
-    X = scipy.misc.imread(Img)
+    # X = scipy.misc.imread(Img)
+    X = imageio.imread(Img)
 
     X = np.double(X)  # converts it to double
     temp = np.copy(X)   # creates a copy for later use
